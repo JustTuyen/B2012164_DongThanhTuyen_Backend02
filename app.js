9bdb0da3cbd6fe17d648911dc97ed1e7c0ffa124
JustTuyen <dongtuyen6202@gmail.com>
2025-11-01 14:09:33 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+/* eslint-disable no-undef */
+
+const ApiError = require('./app/app-error');
 const express = require('express');
 const cors = require('cors');
+const contactsRouter = require('./app/routes/contact.route');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use('/api/contacts', contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// handle errors
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    });
+});
 
 app.get("/", (req ,res)=>{
     res.json({
         message: "welcome to contact book application"
     })
 });
-
 module.exports = app;
